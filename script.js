60f8408d3d4bf005c741b9332e68451efaeb2afa
Abdul Khok <khok.abdul@gmail.com>
2024-02-26 23:05:40 +0200

    feat: added user input detection

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ function checker(input) {
     let num = Math.floor(Math.random() * 3);
 
     document.getElementById("computer_choice").
-    innerHTML = `Computer choose <span>${choices[num].toUpperCase()}</span>`
+    innerHTML = `Computer choose <span>${choices[num].toUpperCase()}</span>`;
+
+    document.getElementById("user_choice").
+    innerHTML = `You choose <span>${input.toUpperCase}</span>`;
 }
