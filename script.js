12ca82de640a98ff8de89681bd21c615437f32a9
Abdul Khok <khok.abdul@gmail.com>
2024-02-26 23:01:53 +0200

    feat: added generator of computer moves

---
+++ script.js
@@ -0,0 +1,7 @@
+function checker(input) {
+    let choices = ["rock", "paper", "scissors"];
+    let num = Math.floor(Math.random() * 3);
+
+    document.getElementById("computer_choice").
+    innerHTML = `Computer choose <span>${choices[num].toUpperCase()}</span>`
+}
